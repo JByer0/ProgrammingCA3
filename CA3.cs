a46364bf09fad64a98da9935aa1d73a3d7547771
JByer0 <s00240739@atu.ie>
2023-04-17 10:09:00 +0100

    Add menu

--- CA3.cs
+++ CA3.cs
@@ -4,7 +4,19 @@ namespace CA3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int choice;
+
+            do
+            {
+                Console.WriteLine("1. Ship Reports");
+                Console.WriteLine("2. Occupation Report");
+                Console.WriteLine("3. Age Report");
+                Console.WriteLine("4. Exit");
+
+                Console.WriteLine("Enter Choice: ");
+                choice = int.Parse(Console.ReadLine());
+
+            } while (choice != 4);
         }
     }
 }
